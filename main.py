ccf8ebb2540857d816aa690e36bc72a89bcc004a
Ivan <Ivan.Kudryavtsev@protonmail.com>
2022-02-26 15:13:53 +0000

    Theoretically correct transcription output

--- main.py
+++ main.py
@@ -3,30 +3,69 @@ import base64
 import pyaudio
 import websockets
 import json
+import asyncio
 import configure
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
 
-
+auth_key = "1fb21cd48fdd4de79c3e7f9e454afb0c"
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.s
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def streamToAPI(stream):
+    URL = "wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000"
 
+    async def send_receive():
+        print(f'Connecting websocket to url ${URL}')
+        async with websockets.connect(
+                URL,
+                extra_headers=(("Authorization", auth_key),),
+                ping_interval=5,
+                ping_timeout=20
+        ) as _ws:
+            await asyncio.sleep(0.1)
+            print("Receiving SessionBegins ...")
+            session_begins = await _ws.recv()
+            print(session_begins)
+            print("Sending messages ...")
+
+            async def send():
+                while True:
+                    try:
+                        data = stream.read(FRAMES_PER_BUFFER)
+                        data = base64.b64encode(data).decode("utf-8")
+                        json_data = json.dumps({"audio_data": str(data)})
+                        await _ws.send(json_data)
+                    except websockets.exceptions.ConnectionClosedError as e:
+                        print(e)
+                        assert e.code == 4008
+                        break
+                    except Exception as e:
+                        assert False, "Not a websocket 4008 error"
+                    await asyncio.sleep(0.01)
+
+                return True
+
+            async def receive():
+                while True:
+                    try:
+                        result_str = await _ws.recv()
+                        print(json.loads(result_str)['text'])
+                    except websockets.exceptions.ConnectionClosedError as e:
+                        print(e)
+                        assert e.code == 4008
+                        break
+                    except Exception as e:
+                        assert False, "Not a websocket 4008 error"
+
+            send_result, receive_result = await asyncio.gather(send(), receive())
 
 def beginStream():
-    FRAMES_PER_BUFFER = 3200
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 16000
     p = pyaudio.PyAudio()
 
     # starts recording
@@ -39,3 +78,8 @@ def beginStream():
     )
 
     return stream
+
+
+if __name__ == '__main__':
+    stream = beginStream()
+    streamToAPI(stream)
