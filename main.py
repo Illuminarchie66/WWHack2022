c8edee746ce1e34682347274c67d1b1a02c92655
Ivan <Ivan.Kudryavtsev@protonmail.com>
2022-02-26 14:01:27 +0000

    Finally installed pyaudio

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 # This is a sample Python script.
+import base64
+import pyaudio
+import websockets
+import json
+import configure
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -14,3 +20,22 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+def beginStream():
+    FRAMES_PER_BUFFER = 3200
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 16000
+    p = pyaudio.PyAudio()
+
+    # starts recording
+    stream = p.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
+        input=True,
+        frames_per_buffer=FRAMES_PER_BUFFER
+    )
+
+    return stream
