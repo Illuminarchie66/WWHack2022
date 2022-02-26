b967d75b727084c441bda4c776d14024936576fb
Illuminarchie66 <54813067+Illuminarchie66@users.noreply.github.com>
2022-02-26 18:57:40 +0000

    Quote to text update

--- main.py
+++ main.py
@@ -1,85 +1,39 @@
-# This is a sample Python script.
-import base64
-import pyaudio
-import websockets
-import json
-import asyncio
-import configure
-FRAMES_PER_BUFFER = 3200
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 16000
+import requests
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
-auth_key = "1fb21cd48fdd4de79c3e7f9e454afb0c"
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.s
+def createLink(input):
+    split = input.split(" ")
+    website = "https://www.quodb.com/search/"
+    for x in split:
+        website = website + x + "%20"
+    website = website[:-3]
+    return website
 
-# Press the green button in the gutter to run the script.
+url = createLink("I am your father")
+webdriver = webdriver.Chrome()
+webdriver.get(url)
+time.sleep(0.002)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-def streamToAPI(stream):
-    URL = "wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000"
+html = BeautifulSoup(webdriver.page_source,'html.parser')
+webdriver.close()
+results = html.find(id="results_table")
 
-    async def send_receive():
-        print(f'Connecting websocket to url ${URL}')
-        async with websockets.connect(
-                URL,
-                extra_headers=(("Authorization", auth_key),),
-                ping_interval=5,
-                ping_timeout=20
-        ) as _ws:
-            await asyncio.sleep(0.1)
-            print("Receiving SessionBegins ...")
-            session_begins = await _ws.recv()
-            print(session_begins)
-            print("Sending messages ...")
+times = html.findAll(class_="editable editable-click editable-empty editable-open time")
+rows = results.findAll(lambda tag: tag.name=='tr')
 
-            async def send():
-                while True:
-                    try:
-                        data = stream.read(FRAMES_PER_BUFFER)
-                        data = base64.b64encode(data).decode("utf-8")
-                        json_data = json.dumps({"audio_data": str(data)})
-                        await _ws.send(json_data)
-                    except websockets.exceptions.ConnectionClosedError as e:
-                        print(e)
-                        assert e.code == 4008
-                        break
-                    except Exception as e:
-                        assert False, "Not a websocket 4008 error"
-                    await asyncio.sleep(0.01)
+arr = []
+for i in range(0, int(len(rows)/2)):
+    arr.append([rows[i*2]['data-title'], times[i].getText()])
 
-                return True
-
-            async def receive():
-                while True:
-                    try:
-                        result_str = await _ws.recv()
-                        print(json.loads(result_str)['text'])
-                    except websockets.exceptions.ConnectionClosedError as e:
-                        print(e)
-                        assert e.code == 4008
-                        break
-                    except Exception as e:
-                        assert False, "Not a websocket 4008 error"
-
-            send_result, receive_result = await asyncio.gather(send(), receive())
-
-def beginStream():
-    p = pyaudio.PyAudio()
-
-    # starts recording
-    stream = p.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=FRAMES_PER_BUFFER
-    )
-
-    return stream
+print(arr)
 
 
-if __name__ == '__main__':
-    stream = beginStream()
-    streamToAPI(stream)
+
+
+
+
+
