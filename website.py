aaf61d59282534edab33b2759d57f44605925c18
Materol <gaarapavel@gmail.com>
2022-02-27 11:38:11 +0000

    Version 1.0 Website fully finished. 	- Added full navigation for the website with the nav bar. 	- A fully detailed about page added. 	- Time stamps for quotes in the films added. 	- A list of all the films added. 	- Added voice audio quotes for testing.

--- website.py
+++ website.py
@@ -8,12 +8,17 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 @app.route("/record", methods=["GET", "POST"])
 def record():
     file = None
     quote="You need to submit a quote first!"
+    time="Can't be a time for a quote that doesn't exist :("
     film="Need a quote first!"
+    array=[]
     if request.method == "POST":
         if "file" not in request.files:
             return redirect(request.url) # we need a resubmission, go back
@@ -25,10 +30,12 @@ def record():
             array = quoter.QuoteToMovie(quote)
             if len(array)==0:
                 film = "Couldn't find a match :("
+                time = "D:"
             else: 
                 film = array[0][0]
+                time = array[0][1]
 
-    return render_template("record.html", quote=quote, film=film)
+    return render_template("record.html", quote=quote, film=film, allFilms=array, time=time)
 
 if __name__ == "__main__":
     app.run()
